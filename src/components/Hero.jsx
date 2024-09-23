7c01a928472f649233a6a884582d4669b4d25f9d
Mayank <2020kucp1006@iiitkota.ac.in>
2024-09-23 17:52:40 +0530

    nitesh--

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
           <div className='w-1 sm:h-80 h-40 violet-gradient'/>       
         </div>
         <div>
-          <h1 className={`${styles.heroHeadText}text-white`}>Hi, I'm <span className='text-[#915eff]'>Mayank</span></h1>
+          <h1 className={`${styles.heroHeadText}text-white`}>Hi, I'm <span className='text-[#915eff]'>Nitesh</span></h1>
           <p className={`${styles.heroSubText}mt-2 text-white-100`}>I develop web applications</p>
         </div>
       </div>
